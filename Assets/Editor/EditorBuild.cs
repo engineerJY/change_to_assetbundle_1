8ab0948865b991310a5f05a229eaa204d739d8e2
横田 准 <jyokota@r6.ucom.ne.jp>
2019-11-04 15:32:34 +0900

    ビルドクラス追加

---
+++ Assets/Editor/EditorBuild.cs
@@ -0,0 +1,69 @@
+﻿using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EditorBuild
+{
+	[MenuItem("EditorBuild/Build AssetBundles")]
+	public static void BuildAllAssetBundles()
+	{
+		string assetBundleDirectory = "AssetBundlesWebGL";
+		if (!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.WebGL);
+
+		assetBundleDirectory = "AssetBundlesiOS";
+		if (!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
+
+		assetBundleDirectory = "AssetBundlesAndroid";
+		if (!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+
+    public static void _BuildAndroid()
+    {
+        /*
+        m_TargetPlatform = BuildTarget.Android;
+		bool status = BuildAndroid(m_IsRelease);
+		EditorApplication.Exit(status ? 0 : 1);
+        */
+
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+
+        List<string> allScene = new List<string>();
+
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                //Debug.Log(scene.path);
+                allScene.Add(scene.path);
+            }
+        }
+
+        PlayerSettings.applicationIdentifier = "com.EngineerBlog.GameAction2";
+        PlayerSettings.statusBarHidden = true;
+        var a = BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "/Documentation/Build/Android/gameaction2.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/AssetLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator LoadAsset()
+    {
+        //UnityWebRequestAssetBundle.GetAssetBundle();
+
+        yield break;
+    }
+}
