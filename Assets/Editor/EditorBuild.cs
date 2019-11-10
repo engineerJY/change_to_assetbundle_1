cb5c4dcb17738aff820ce13f9db5e892cdb38151
横田 准 <jyokota@r6.ucom.ne.jp>
2019-11-10 15:20:52 +0900

    アセットバンドルビルド修正

--- Assets/Editor/EditorBuild.cs
+++ Assets/Editor/EditorBuild.cs
@@ -5,24 +5,10 @@ using System.Collections.Generic;
 
 public class EditorBuild
 {
-	[MenuItem("EditorBuild/Build AssetBundles")]
-	public static void BuildAllAssetBundles()
+	[MenuItem("EditorBuild/BuildAndroidAssetBundles")]
+	public static void BuildAndroidAssetBundles()
 	{
-		string assetBundleDirectory = "AssetBundlesWebGL";
-		if (!Directory.Exists(assetBundleDirectory))
-		{
-			Directory.CreateDirectory(assetBundleDirectory);
-		}
-		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.WebGL);
-
-		assetBundleDirectory = "AssetBundlesiOS";
-		if (!Directory.Exists(assetBundleDirectory))
-		{
-			Directory.CreateDirectory(assetBundleDirectory);
-		}
-		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
-
-		assetBundleDirectory = "AssetBundlesAndroid";
+		string assetBundleDirectory = "/Documentation/Build/AssetBundle/ChangeToAssetBundle1/Android";
 		if (!Directory.Exists(assetBundleDirectory))
 		{
 			Directory.CreateDirectory(assetBundleDirectory);
@@ -30,7 +16,29 @@ public class EditorBuild
 		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
 	}
 
-    public static void _BuildAndroid()
+    [MenuItem("EditorBuild/BuildiOSAssetBundles")]
+    public static void BuildiOSAssetBundles()
+    {
+        string assetBundleDirectory = "/Documentation/Build/AssetBundle/ChangeToAssetBundle1/iOS";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
+    }
+
+    [MenuItem("EditorBuild/BuildWebGLAssetBundles")]
+    public static void BuildWebGLAssetBundles()
+    {
+        string assetBundleDirectory = "/Documentation/Build/AssetBundle/ChangeToAssetBundle1/WebGL";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.WebGL);
+    }
+
+    public static void BuildAndroid()
     {
         /*
         m_TargetPlatform = BuildTarget.Android;
