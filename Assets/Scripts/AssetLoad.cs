b326d0426a5fe621c7939f53ff55f5f2c0c7d09f
横田 准 <jyokota@r6.ucom.ne.jp>
2019-12-31 01:39:20 +0900

    アセットバンドルロード修正

--- Assets/Scripts/AssetLoad.cs
+++ Assets/Scripts/AssetLoad.cs
@@ -6,15 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class AssetLoad : SingletonMonoBehaviour<AssetLoad>
 {
-    public Dictionary<string, Texture> assetsTextures = new Dictionary<string, Texture>();
-    public Dictionary<string, Shader> assetsShaders = new Dictionary<string, Shader>();
-    public Dictionary<string, Material> assetsMaterials = new Dictionary<string, Material>();
-    public Dictionary<string, Object> assetsObjects = new Dictionary<string, Object>();
-    public Dictionary<string, Animation> assetsAnimations = new Dictionary<string, Animation>();
-    public Dictionary<string, Animator> assetsAnimators = new Dictionary<string, Animator>();
-    public Dictionary<string, AudioClip> assetsAudioClips = new Dictionary<string, AudioClip>();
-    public Dictionary<string, GameObject> assetsPrefabs = new Dictionary<string, GameObject>();
+    /// <summary>
+    /// ダウンロードURL
+    /// </summary>
+    const string DOWNLOAD_URL = "http://118.27.18.220/assetbundles/ChangeToAssetBundle1";
 
+    /// <summary>
+    /// アセットバンドルの型
+    /// </summary>
     public enum ObjectType
     {
         Animation,
@@ -27,10 +26,14 @@ public class AssetLoad : SingletonMonoBehaviour<AssetLoad>
         Prefab,
     }
 
-
-    const string DOWNLOAD_URL = "http://118.27.18.220/assetbundles/ChangeToAssetBundle1";
+    /// <summary>
+    /// Prefabのアセットバンドルを保持
+    /// </summary>
+    public Dictionary<string, GameObject> assetsPrefabs = new Dictionary<string, GameObject>();
+    
     IEnumerator Start()
     {
+        //Prefabが参照するアセットバンドルをダウンロード
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/texture.pack", ObjectType.Texture));
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/audio.pack", ObjectType.AudioClip));
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/shader.pack", ObjectType.Shader));
@@ -38,12 +41,20 @@ public class AssetLoad : SingletonMonoBehaviour<AssetLoad>
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/model.pack", ObjectType.Model));
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/animation.pack", ObjectType.Animation));
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/animator.pack", ObjectType.Animator));
-        
+
+        //Prefabのアセットバンドルをダウンロード
         yield return StartCoroutine(LoadAsset(DOWNLOAD_URL + "/prefab.pack", ObjectType.Prefab));
 
+        //アセットバンドルをすべてダウンロード完了したらゲームシーンを加算でロード
         SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Additive);
     }
 
+    /// <summary>
+    /// アセットバンドルをダウンロードし、PrefabはDictionaryに格納
+    /// </summary>
+    /// <param name="url">ダウンロードURL</param>
+    /// <param name="type">アセットバンドルの型</param>
+    /// <returns></returns>
     IEnumerator LoadAsset(string url, ObjectType type)
     {
         var request = UnityWebRequestAssetBundle.GetAssetBundle(url);
@@ -57,74 +68,40 @@ public class AssetLoad : SingletonMonoBehaviour<AssetLoad>
 
         Debug.Log("load done!");
 
-        var assetBundleA = DownloadHandlerAssetBundle.GetContent(request);
+        var assetBundle = DownloadHandlerAssetBundle.GetContent(request);
 
         switch(type)
         {
-            case
-            ObjectType.Animation:
-                var assets = assetBundleA.LoadAllAssets<Animation>();
-
+            case ObjectType.Animation: break;
+            case ObjectType.Animator: break;
+            case ObjectType.AudioClip: break;
+            case ObjectType.Shader: break;
+            case ObjectType.Texture: break;
+            case ObjectType.Material: break;
+            case ObjectType.Model: break;
+                
+            case ObjectType.Prefab:
+                var assets = assetBundle.LoadAllAssets<GameObject>();
                 foreach (var a in assets)
-                {
-                    Debug.Log(a.name);
-                    assetsAnimations.Add(a.name, a);
-                }
-                break;
-
-            case
-        ObjectType.Animator:
-                var assets2 = assetBundleA.LoadAllAssets<Animator>();
-
-                foreach (var a in assets2)
-                {
-                    Debug.Log(a.name);
-                    assetsAnimators.Add(a.name, a);
-                }
-                break;
-
-            case
-        ObjectType.AudioClip:
-                break;
-
-            case
-        ObjectType.Shader:
-                break;
-
-            case
-        ObjectType.Texture:
-                break;
-
-            case
-        ObjectType.Material:
-                break;
-
-            case
-        ObjectType.Model:
-                break;
-
-            case
-        ObjectType.Prefab:
-                var assets8 = assetBundleA.LoadAllAssets<GameObject>();
-
-                foreach (var a in assets8)
                 {
                     Debug.Log(a.name);
                     assetsPrefabs.Add(a.name, a);
                 }
                 break;
-
         }
-
-
-        
-
-        //var audioA = assetBundleA.LoadAsset<AudioClip>("SFXHouseAmbience.wav");
-
-        //Debug.Log(audioA == null);
-
-        //Debug.Log(audioA.name);
-
         yield break;
     }
 }
+
+
+
+/*
+ *
+ public Dictionary<string, Texture> assetsTextures = new Dictionary<string, Texture>();
+    public Dictionary<string, Shader> assetsShaders = new Dictionary<string, Shader>();
+    public Dictionary<string, Material> assetsMaterials = new Dictionary<string, Material>();
+    public Dictionary<string, Object> assetsObjects = new Dictionary<string, Object>();
+    public Dictionary<string, Animation> assetsAnimations = new Dictionary<string, Animation>();
+    public Dictionary<string, Animator> assetsAnimators = new Dictionary<string, Animator>();
+    public Dictionary<string, AudioClip> assetsAudioClips = new Dictionary<string, AudioClip>();
+ */
--- Assets/Scripts/MainSceneEntry.cs
+++ Assets/Scripts/MainSceneEntry.cs
@@ -6,20 +6,76 @@ using UnityEngine.UI;
 public class MainSceneEntry : SingletonMonoBehaviour<MainSceneEntry>
 {
     [SerializeField]
+    Transform parent = null;
+
+    [SerializeField]
+    Transform enemyParent = null;
+
+    [SerializeField]
+    Cinemachine.CinemachineVirtualCamera virtualCamera = null;
+
+    [SerializeField]
     Image fade = null;
 
+    /// <summary>
+    /// 実体化するPrefabの配置情報クラス
+    /// </summary>
+    class InstantiateAsset
+    {
+        public string assetName;
+        public Vector3 position;
+        public Vector3 rotation;
+        public Transform parent;
 
+        public InstantiateAsset(string assetName, Vector3 position, Vector3 rotation, Transform parent)
+        {
+            this.assetName = assetName;
+            this.position = position;
+            this.rotation = rotation;
+            this.parent = parent;
+        }
+    }
 
-
+    List<InstantiateAsset> assetList = new List<InstantiateAsset>();
 
     void Awake()
     {
         Debug.Log("MainSecneEntry Awake");
 
-        //JohnLemon
-        var go = Instantiate(AssetLoad.Instance.assetsPrefabs["JohnLemon"]);
+        //実体化するPrefabの座標などを設定
+        assetList.Add(new InstantiateAsset("JohnLemon", new Vector3(1.254f, -0.7138f, -0.894f), new Vector3(0f, 0f, 0f), parent));
 
+        assetList.Add(new InstantiateAsset("Level", new Vector3(11.054f, -0.7138002f, 2.306f), new Vector3(0f, 0f, 0f), parent));
 
+        assetList.Add(new InstantiateAsset("Gargoyle", new Vector3(-15.2f, 0f, 0.8f), new Vector3(0f, 135f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Gargoyle", new Vector3(-2.6f, 0f, -8.5f), new Vector3(0f, 30f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Gargoyle", new Vector3(-4.8f, 0f, 10.6f), new Vector3(0f, 135f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Ghost", new Vector3(-5.3f, 0f, -3.1f), new Vector3(0f, 0f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Ghost", new Vector3(1.5f, 0f, 4f), new Vector3(0f, 0f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Ghost", new Vector3(3.2f, 0f, 6.5f), new Vector3(0f, 0f, 0f), enemyParent));
+        assetList.Add(new InstantiateAsset("Ghost", new Vector3(7.4f, 0f, -3f), new Vector3(0f, 0f, 0f), enemyParent));
+
+        assetList.Add(new InstantiateAsset("GameEnding", new Vector3(29.054f, 0.2861998f, 3.806f), new Vector3(0f, 0f, 0f), parent));
+        assetList.Add(new InstantiateAsset("FaderCanvas", new Vector3(477.554f, 261.7862f, 2.306f), new Vector3(0f, 0f, 0f), parent));
+
+        var johnLemonTransform = this.transform;
+        //Prefabを実体化
+        foreach (var asset in assetList)
+        {
+            var go = Instantiate(AssetLoad.Instance.assetsPrefabs[asset.assetName], asset.position, Quaternion.Euler(asset.rotation), asset.parent);
+            go.transform.localPosition = asset.position;
+            
+            //JohnLemonのとき、transformを取得
+            if(asset.assetName == "JohnLemon")
+            {
+                johnLemonTransform = go.transform;
+            }
+        }
+
+        //Cinemachineのカメラ追随ターゲットにJohnLemonを設定
+        virtualCamera.Follow = johnLemonTransform;
+
+        //フェードイン
         fade.gameObject.SetActive(false);
     }
 
